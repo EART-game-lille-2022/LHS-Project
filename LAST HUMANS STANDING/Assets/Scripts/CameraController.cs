43b4cc27f3c7cf02478f677f229ef239a6f4e8ce
Sybirk <115795460+Sybirk@users.noreply.github.com>
2023-03-31 14:27:08 +0200

    anima cam + bool Onplay

--- LAST HUMANS STANDING/Assets/Scripts/CameraController.cs
+++ LAST HUMANS STANDING/Assets/Scripts/CameraController.cs
@@ -8,9 +8,13 @@ public class CameraController : MonoBehaviour
     public Transform camTarget;
     public float delayed;
     public Vector3 PositionOffSet;
+    public Ease ease = Ease.InOutBounce;
+    public float time;
     public void UpdateCam(TurnBasedBehaviour targeted )
     {
         camTarget.position = new Vector3(targeted.transform.position.x,targeted.transform.position.y,camTarget.position.z);
-        transform.DOMove(camTarget.position, 0.5f);
+        // transform.DOMove(camTarget.position, time).SetEase(ease);
+        transform.DOMoveX(camTarget.position.x, time).SetEase(Ease.Linear);
+        transform.DOMoveY(camTarget.position.y, time).SetEase(ease);
     }
 }
